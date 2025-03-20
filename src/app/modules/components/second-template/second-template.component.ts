59ab80dc4b33e3e3da16edd374d3773e9afd05f0
Fejin Francis <fejinfm2000@gmail.com>
2025-03-20 10:48:43 +0530

    Added static json structure

--- src/app/modules/components/second-template/second-template.component.ts
+++ src/app/modules/components/second-template/second-template.component.ts
@@ -15,43 +15,73 @@ import { CommonModule } from '@angular/common';
   styleUrl: './second-template.component.scss'
 })
 export class SecondTemplateComponent {
-  experienceCategories: ExperienceCategory[] = [
-    {
-      title: 'Frontend Development',
-      skills: [
-        { name: 'HTML', level: 'Experienced' },
-        { name: 'CSS', level: 'Experienced' },
-        { name: 'SASS', level: 'Intermediate' },
-        { name: 'JavaScript', level: 'Basic' },
-        { name: 'TypeScript', level: 'Basic' },
-        { name: 'Material UI', level: 'Intermediate' }
-      ]
-    },
-    {
-      title: 'Backend Development',
-      skills: [
-        { name: 'PostgreSQL', level: 'Basic' },
-        { name: 'Node JS', level: 'Intermediate' },
-        { name: 'Express JS', level: 'Intermediate' },
-        { name: 'Git', level: 'Intermediate' }
-      ]
-    }
-  ];
-
-  projectCategories = [
-    {
-      title: 'Project One',
-      des: 'Project One',
-    },
-    {
-      title: 'Project Two',
-      des: 'Project One',
-    },
-    {
-      title: 'Project Three',
-      des: 'Project One',
-    }
-  ];
+  resumeDetails = {
+    name: "Fejin FM",
+    greeting: "Hello, I'm",
+    role: "Full Stack Developer",
+    resumeLink: "https://drive.google.com/file/d/1GqB_UUYArKCdhLVpV5sMBpHX6i_p7nVC/view?usp=sharing",
+    linkedInUrl: "https://www.linkedin.com/in/your-profile",
+    githubUrl: "https://github.com/",
+    aboutImage: " ",
+    educationList: ["BE. Computer Science Engineering Degree"],
+    experienceList: ['3+ years in Full Stack Development'],
+    aboutMeContentList: [
+      "Ever since I wrote my first line of code, I’ve been passionate about solving real-world problems through technology. With a deep interest in software development, I started my journey by exploring various programming languages and frameworks.",
+      "Over the years, I have honed my skills in Full Stack Development, specializing in Angular, Java, Spring Boot, and SQL/MongoDB. I have worked on high-performance web applications, ensuring scalability and security while delivering an intuitive user experience.",
+      "Beyond coding, I enjoy collaborating with teams, learning new technologies, and implementing best practices in DevOps and software architecture. My goal is to build robust and efficient solutions that make a difference.",
+      "In my free time, I love exploring open-source projects, mentoring aspiring developers, and staying updated with the latest trends in web development and cloud computing.",
+      "Let’s connect and build something amazing together! 🚀"
+    ],
+    mailTo: "fejinfm2000@gmail.com",
+    email: "fejinfm2000@gmail.com",
+    experienceCategories: [
+      {
+        title: 'Frontend Development',
+        skills: [
+          { name: 'HTML', level: 'Experienced' },
+          { name: 'CSS', level: 'Experienced' },
+          { name: 'SASS', level: 'Intermediate' },
+          { name: 'JavaScript', level: 'Basic' },
+          { name: 'TypeScript', level: 'Basic' },
+          { name: 'Material UI', level: 'Intermediate' }
+        ]
+      },
+      {
+        title: 'Backend Development',
+        skills: [
+          { name: 'Java', level: 'Intermediate' },
+          { name: 'Git', level: 'Intermediate' }
+        ]
+      },
+      {
+        title: 'Data Base',
+        skills: [
+          { name: 'SQL', level: 'Intermediate' },
+          { name: 'Express JS', level: 'Intermediate' },
+        ]
+      }
+    ],
+    projectCategories: [
+      {
+        title: 'Progress Tracker Web App',
+        des: 'A scalable and high-performance progress tracking application using Angular and Spring Boot.',
+        gitLink: "",
+        demoLink: ""
+      },
+      {
+        title: 'Banking Web Application',
+        des: 'A secure banking application with role-based authentication and transaction management.',
+        gitLink: "",
+        demoLink: ""
+      },
+      {
+        title: 'Portfolio Template page',
+        des: 'A scalable Portfolio Template view only web page application using Angular.',
+        gitLink: "",
+        demoLink: ""
+      }
+    ]
+  };
   onImageError(event: any, imageName: string) {
     event.target.src = `assets/images/${imageName}.webp`;
   }
